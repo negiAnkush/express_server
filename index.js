c719f4914104e920b8f6fecb7cde09c7980220ad
Ankush Negi <ankush.negi.ca@gmail.com>
2023-08-20 22:14:17 -0400

    adding express get code

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+    res.send("<h1>welcome to the express page</h1>");
+    console.log("msg send to client");
+});
+
 app.listen(3000, () => {
     console.log(`server is running on ${port}  `)
 });
